d9e64aa37c2ca48b927a59bb2422b5f8b0804e2f
Alanis-C3 <acastillo3@ufl.edu>
2025-08-28 13:33:05 -0400

    fixed supabase client, created .env and login page did function for auth and basic front end

--- Backend/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-    const supabaseUrl = "https://knmxugsodrxfpfpnykxp.supabase.co";
-    const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtubXh1Z3NvZHJ4ZnBmcG55a3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MTM2MTYsImV4cCI6MjA3MTE4OTYxNn0.GwCoHC3zHstmkMBKe_JM8jNmN-j-pqpQEzUA3LnY66Q";
-
-    export const supabase = createClient(supabaseUrl, supabaseAnonKey);
